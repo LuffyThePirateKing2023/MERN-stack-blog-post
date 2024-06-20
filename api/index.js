1220730137ed55005901ec4ff42e6626930de31f
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-06-20 17:52:25 +0800

    udpate: api index.js

--- api/index.js
+++ api/index.js
@@ -42,11 +42,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-// app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
