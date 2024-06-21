d3a69385c6db4d438d8b339fcba6fee23e108a75
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-06-21 17:12:11 +0800

    add access_token for vercel

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,7 +1,11 @@
 import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+  const token = req.cookies.access_token || process.env.Vercel_access_token;
   if (!token) {
     return next(errorHandler(401, 'Unauthorized'));
   }
